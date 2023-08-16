6965b07d85e017fcc3c9c1fdcfb292d70a210333
heoseungyeon <dia0312@naver.com>
2023-08-16 20:34:10 +0900

    init[SY]: 프로젝트 초기 세팅

---
+++ hsy/sybatch/src/main/java/com/sipe/sybatch/SybatchApplication.java
@@ -0,0 +1,13 @@
+package com.sipe.sybatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SybatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SybatchApplication.class, args);
+	}
+
+}
